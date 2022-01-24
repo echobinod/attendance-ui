05515b0965fca13190b5c5e37121d6c4ddd63b8b
Binod Shahi <binod@Binods-MacBook-Air.local>
2022-01-25 00:30:34 +0545

    App layout setup

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+export default {
+  name: "BaseLayout",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
---
+++ src/layout/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <footer
+      class="text-center text-white fixed-bottom"
+      style="background-color: #21081a"
+    >
+      <div class="container p-4"></div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        <a class="text-white" href="https://github.com/echobinod">@echobinod</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
